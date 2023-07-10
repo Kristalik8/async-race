64c5592658a2c4e588ef4b51ae912ae8ea6c6ac3
Kristina <nice.kres@mail.ru>
2023-07-10 16:08:29 +0300

    feat: add data car brands and models

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
-import '@babel/polyfill';
 import './index.scss'
+import './ts/dataCars'
+const baseUrl = 'http://localhost:3000';
+const garage = `${baseUrl}/garage`;
+const winners = `${baseUrl}/winners`;
+const engine = `${baseUrl}/engine`;
+console.log(engine)
+const garageSelector = document.querySelector('.garage')
+export interface ICar {
+    name: string;
+    color: string;
+    id: number;
+}
+export interface IGarage {
+    items: ICar[];
+    count: number;
+}
+
+const getGarage = async (page: number): Promise<IGarage> => {
+    const res = await fetch(`${garage}?_page=${page}&_limit=7`);
+    return {
+        items: await res.json(),
+        count: Number(res.headers.get('X-Total-Count')),
+    };
+};
+
+const getCar = async (id: number): Promise<ICar> => (await fetch(`${garage}/${id}`)).json();
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,113 +1,56 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const mode = process.env.NODE_ENV || 'development';
-const devMode = mode === 'development';
-const target = devMode ? 'web' : 'browserslist';
-const devtool = devMode ? 'source-map' : undefined;
+const isProduction = process.env.NODE_ENV === 'production';
 
 
-module.exports = {
-    mode,
-    target,
-    devtool,
-    devServer: {
-    port: 3000,
-    open: true,
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
+
+
+const config = {
+    mode: isProduction ? 'production': 'development',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
     },
-entry: ["@babel/polyfill", path.resolve(__dirname, 'src', 'index.ts')],
-output: {
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    filename: '[name].[contenthash].js',
-    assetModuleFilename: 'assets/[hash][ext]'
-},
-plugins: [
-    new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'src', "index.html")
-    }),
-    new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css',
-    }),
-],
-module: {
-    rules: [
-        {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        },
-        {
-            test: /\.html$/i,
-            loader: 'html-loader',
-        },
-        {
-            test: /\.(c|sa|sc)ss$/i,
-            use: [
-                devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-                "css-loader",
-                {
-                    loader: 'postcss-loader',
-                    options: {
-                        postcssOptions: {
-                            plugins: [require('postcss-preset-env')],
-                        }
-                    }
-                },
-                'sass-loader'
-            ],
-        },
-        {
-            test: /\.woff2?$/i,
-            type: 'asset/resource',
-            generator: {
-                filename: 'fonts/[name][ext]'
-            }
-        },
-        {
-            test: /\.(jpe?g|png|webp|gif|svg)$/i,
-            use: [
-                {
-                    loader: 'image-webpack-loader',
-                    options: {
-                        mozjpeg: {
-                        progressive: true,
-                        },
-                        optipng: {
-                        enabled: false,
-                        },
-                        pngquant: {
-                        quality: [0.65, 0.90],
-                        speed: 4
-                        },
-                        gifsicle: {
-                        interlaced: false,
-                        },
-                        webp: {
-                        quality: 75
-                        }
-                    }
-                }
-            ],
-            type: 'asset/resource',
-        },
-        {
-            test: /\.(?:js|mjs|cjs)$/,
-            exclude: /node_modules/,
-            use: {
-            loader: 'babel-loader',
-            options: {
-                presets: [
-                ['@babel/preset-env', { targets: "defaults" }]
-                ]
-            }
-            }
-        },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/'],
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler,'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [stylesHandler, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+};
 
-    ]
-},
-resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-},
-}
+module.exports = () => {
+    if (isProduction)  config.plugins.push(new MiniCssExtractPlugin());
+    return config;
+};
